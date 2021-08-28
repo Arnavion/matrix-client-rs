e623f5310cf1e49ed175504e32fe6f353d07f2f2
Arnavion <me@arnavion.dev>
2021-08-28 16:27:58 -0700

    Fix parsing optional fields in sync response.

--- src/controller.rs
+++ src/controller.rs
@@ -59,15 +59,17 @@ async fn run_inner(user_id: String) -> anyhow::Result<()> {
 	loop {
 		#[derive(serde::Deserialize)]
 		struct SyncResponse {
-			account_data: crate::SyncResponse_AccountData,
+			account_data: Option<crate::SyncResponse_AccountData>,
 			next_batch: String,
-			rooms: SyncResponse_Rooms,
+			rooms: Option<SyncResponse_Rooms>,
 		}
 
 		#[allow(non_camel_case_types)]
 		#[derive(serde::Deserialize)]
 		struct SyncResponse_Rooms {
+			#[serde(default)]
 			join: std::collections::BTreeMap<String, SyncResponse_JoinedRoom>,
+			#[serde(default)]
 			leave: std::collections::BTreeMap<String, SyncResponse_LeftRoom>,
 		}
 
@@ -161,168 +163,172 @@ async fn run_inner(user_id: String) -> anyhow::Result<()> {
 
 		sync_next_batch = Some(sync.next_batch);
 
-		for crate::SyncResponse_AccountData_Event { r#type, content } in sync.account_data.events {
-			let event = crate::AccountDataEvent::parse(r#type, content).context("could not parse event")?;
-			match event {
-				crate::AccountDataEvent::M_SecretStorage_DefaultKey { key } => {
-					#[allow(unused)] // TODO
-					{
-						default_key_id = Some(key);
-					}
-				},
-
-				crate::AccountDataEvent::M_SecretStorage_Key { id, description: crate::KeyDescription { algorithm, name, passphrase } } => {
-					let mut state = state_manager.load().context("could not load state")?;
-
-					if let Some(secret_storage_key) = state.secret_storage_keys.remove(&id) {
-						let crate::KeyDescription_Algorithm::AesHmacSha2 { iv, mac } = algorithm;
-
-						let key = match (secret_storage_key, passphrase) {
-							(crate::state::SecretStorageKey::Passphrase(state_passphrase), Some(event_passphrase)) => {
-								let crate::KeyDescription_Passphrase::Pbkdf2 { bits, salt, iterations } = event_passphrase;
-								crate::aes_hmac_sha2::derive_key_from_passphrase(
-									state_passphrase.as_bytes(),
-									bits,
-									&salt,
-									iterations,
-									&iv,
-									&mac,
-								).context("could not derive secret storage key from passphrase")?
-							},
-
-							(crate::state::SecretStorageKey::Keyfile(keyfile), _) =>
-								crate::aes_hmac_sha2::derive_key_from_keyfile(keyfile, &iv, &mac).context("could not decode server backup recovery key")?,
-
-							_ => continue,
-						};
-						keys.insert(id, (name, key));
-					}
-				},
-
-				crate::AccountDataEvent::M_MegolmBackup_V1 { encrypted } =>
-					for (key_id, secret) in encrypted {
-						if let Some((_, key)) = keys.get(&key_id) {
-							let crate::AesHmacSha2Secret { ciphertext, iv, mac } =
-								secret.into_aes_hmac_sha2()
-								.context("could not parse m.megolm_backup.v1 secret")?;
-							let mut stream = base64::decode(ciphertext).context("could not parse m.megolm_backup.v1 secret")?;
-							let () =
-								crate::aes_hmac_sha2::decrypt(
-									key,
-									"m.megolm_backup.v1",
-									&mut stream,
-									&iv,
-									&mac,
-								)
-								.context("could not parse m.megolm_backup.v1 secret")?;
-							#[allow(unused)] // TODO
-							{
-								backup_key = Some(base64::decode(&stream).context("could not parse decrypted m.megolm_backup.v1 secret")?);
-							}
+		if let Some(account_data) = sync.account_data {
+			for crate::SyncResponse_AccountData_Event { r#type, content } in account_data.events {
+				let event = crate::AccountDataEvent::parse(r#type, content).context("could not parse event")?;
+				match event {
+					crate::AccountDataEvent::M_SecretStorage_DefaultKey { key } => {
+						#[allow(unused)] // TODO
+						{
+							default_key_id = Some(key);
 						}
 					},
 
-				crate::AccountDataEvent::M_CrossSigning_Master { encrypted } =>
-					for (key_id, secret) in encrypted {
-						if let Some((_, key)) = keys.get(&key_id) {
-							let crate::AesHmacSha2Secret { ciphertext, iv, mac } =
-								secret.into_aes_hmac_sha2()
-								.context("could not parse m.cross_signing.master secret")?;
-							let mut stream = base64::decode(ciphertext).context("could not parse m.cross_signing.master secret")?;
-							let () =
-								crate::aes_hmac_sha2::decrypt(
-									key,
-									"m.cross_signing.master",
-									&mut stream,
-									&iv,
-									&mac,
-								)
-								.context("could not parse m.cross_signing.master secret")?;
-							#[allow(unused)] // TODO
-							{
-								cross_signing_master_key = Some(base64::decode(&stream).context("could not parse decrypted m.cross_signing.master secret")?);
-							}
+					crate::AccountDataEvent::M_SecretStorage_Key { id, description: crate::KeyDescription { algorithm, name, passphrase } } => {
+						let mut state = state_manager.load().context("could not load state")?;
+
+						if let Some(secret_storage_key) = state.secret_storage_keys.remove(&id) {
+							let crate::KeyDescription_Algorithm::AesHmacSha2 { iv, mac } = algorithm;
+
+							let key = match (secret_storage_key, passphrase) {
+								(crate::state::SecretStorageKey::Passphrase(state_passphrase), Some(event_passphrase)) => {
+									let crate::KeyDescription_Passphrase::Pbkdf2 { bits, salt, iterations } = event_passphrase;
+									crate::aes_hmac_sha2::derive_key_from_passphrase(
+										state_passphrase.as_bytes(),
+										bits,
+										&salt,
+										iterations,
+										&iv,
+										&mac,
+									).context("could not derive secret storage key from passphrase")?
+								},
+
+								(crate::state::SecretStorageKey::Keyfile(keyfile), _) =>
+									crate::aes_hmac_sha2::derive_key_from_keyfile(keyfile, &iv, &mac).context("could not decode server backup recovery key")?,
+
+								_ => continue,
+							};
+							keys.insert(id, (name, key));
 						}
 					},
 
-				crate::AccountDataEvent::M_CrossSigning_SelfSigning { encrypted } =>
-					for (key_id, secret) in encrypted {
-						if let Some((_, key)) = keys.get(&key_id) {
-							let crate::AesHmacSha2Secret { ciphertext, iv, mac } =
-								secret.into_aes_hmac_sha2()
-								.context("could not parse m.cross_signing.self_signing secret")?;
-							let mut stream = base64::decode(ciphertext).context("could not parse m.cross_signing.self_signing secret")?;
-							let () =
-								crate::aes_hmac_sha2::decrypt(
-									key,
-									"m.cross_signing.self_signing",
-									&mut stream,
-									&iv,
-									&mac,
-								)
-								.context("could not parse m.cross_signing.self_signing secret")?;
-							#[allow(unused)] // TODO
-							{
-								cross_signing_self_signing_key = Some(base64::decode(&stream).context("could not parse decrypted m.cross_signing.self_signing secret")?);
+					crate::AccountDataEvent::M_MegolmBackup_V1 { encrypted } =>
+						for (key_id, secret) in encrypted {
+							if let Some((_, key)) = keys.get(&key_id) {
+								let crate::AesHmacSha2Secret { ciphertext, iv, mac } =
+									secret.into_aes_hmac_sha2()
+									.context("could not parse m.megolm_backup.v1 secret")?;
+								let mut stream = base64::decode(ciphertext).context("could not parse m.megolm_backup.v1 secret")?;
+								let () =
+									crate::aes_hmac_sha2::decrypt(
+										key,
+										"m.megolm_backup.v1",
+										&mut stream,
+										&iv,
+										&mac,
+									)
+									.context("could not parse m.megolm_backup.v1 secret")?;
+								#[allow(unused)] // TODO
+								{
+									backup_key = Some(base64::decode(&stream).context("could not parse decrypted m.megolm_backup.v1 secret")?);
+								}
 							}
-						}
-					},
+						},
 
-				crate::AccountDataEvent::M_CrossSigning_UserSigning { encrypted } =>
-					for (key_id, secret) in encrypted {
-						if let Some((_, key)) = keys.get(&key_id) {
-							let crate::AesHmacSha2Secret { ciphertext, iv, mac } =
-								secret.into_aes_hmac_sha2()
-								.context("could not parse m.cross_signing.user_signing secret")?;
-							let mut stream = base64::decode(ciphertext).context("could not parse m.cross_signing.user_signing secret")?;
-							let () =
-								crate::aes_hmac_sha2::decrypt(
-									key,
-									"m.cross_signing.user_signing",
-									&mut stream,
-									&iv,
-									&mac,
-								)
-								.context("could not parse m.cross_signing.user_signing secret")?;
-							#[allow(unused)] // TODO
-							{
-								cross_signing_user_signing_key = Some(base64::decode(&stream).context("could not parse decrypted m.cross_signing.user_signing secret")?);
+					crate::AccountDataEvent::M_CrossSigning_Master { encrypted } =>
+						for (key_id, secret) in encrypted {
+							if let Some((_, key)) = keys.get(&key_id) {
+								let crate::AesHmacSha2Secret { ciphertext, iv, mac } =
+									secret.into_aes_hmac_sha2()
+									.context("could not parse m.cross_signing.master secret")?;
+								let mut stream = base64::decode(ciphertext).context("could not parse m.cross_signing.master secret")?;
+								let () =
+									crate::aes_hmac_sha2::decrypt(
+										key,
+										"m.cross_signing.master",
+										&mut stream,
+										&iv,
+										&mac,
+									)
+									.context("could not parse m.cross_signing.master secret")?;
+								#[allow(unused)] // TODO
+								{
+									cross_signing_master_key = Some(base64::decode(&stream).context("could not parse decrypted m.cross_signing.master secret")?);
+								}
 							}
-						}
-					},
+						},
 
-				crate::AccountDataEvent::Unknown { r#type: _, content: _ } => (),
+					crate::AccountDataEvent::M_CrossSigning_SelfSigning { encrypted } =>
+						for (key_id, secret) in encrypted {
+							if let Some((_, key)) = keys.get(&key_id) {
+								let crate::AesHmacSha2Secret { ciphertext, iv, mac } =
+									secret.into_aes_hmac_sha2()
+									.context("could not parse m.cross_signing.self_signing secret")?;
+								let mut stream = base64::decode(ciphertext).context("could not parse m.cross_signing.self_signing secret")?;
+								let () =
+									crate::aes_hmac_sha2::decrypt(
+										key,
+										"m.cross_signing.self_signing",
+										&mut stream,
+										&iv,
+										&mac,
+									)
+									.context("could not parse m.cross_signing.self_signing secret")?;
+								#[allow(unused)] // TODO
+								{
+									cross_signing_self_signing_key = Some(base64::decode(&stream).context("could not parse decrypted m.cross_signing.self_signing secret")?);
+								}
+							}
+						},
+
+					crate::AccountDataEvent::M_CrossSigning_UserSigning { encrypted } =>
+						for (key_id, secret) in encrypted {
+							if let Some((_, key)) = keys.get(&key_id) {
+								let crate::AesHmacSha2Secret { ciphertext, iv, mac } =
+									secret.into_aes_hmac_sha2()
+									.context("could not parse m.cross_signing.user_signing secret")?;
+								let mut stream = base64::decode(ciphertext).context("could not parse m.cross_signing.user_signing secret")?;
+								let () =
+									crate::aes_hmac_sha2::decrypt(
+										key,
+										"m.cross_signing.user_signing",
+										&mut stream,
+										&iv,
+										&mac,
+									)
+									.context("could not parse m.cross_signing.user_signing secret")?;
+								#[allow(unused)] // TODO
+								{
+									cross_signing_user_signing_key = Some(base64::decode(&stream).context("could not parse decrypted m.cross_signing.user_signing secret")?);
+								}
+							}
+						},
+
+					crate::AccountDataEvent::Unknown { r#type: _, content: _ } => (),
+				}
 			}
 		}
 
 		let mut to_write: std::collections::BTreeMap<String, Vec<crate::RoomViewLine>> = Default::default();
 
-		for (room_id, room) in sync.rooms.join {
-			let lines = to_write.entry(room_id).or_default();
+		if let Some(rooms) = sync.rooms {
+			for (room_id, room) in rooms.join {
+				let lines = to_write.entry(room_id).or_default();
 
-			lines.push(crate::RoomViewLine::Summary { summary: room.summary });
-			lines.push(crate::RoomViewLine::AccountData { account_data: room.account_data });
+				lines.push(crate::RoomViewLine::Summary { summary: room.summary });
+				lines.push(crate::RoomViewLine::AccountData { account_data: room.account_data });
 
-			for event in room.state.events {
-				lines.push(crate::RoomViewLine::State { event });
+				for event in room.state.events {
+					lines.push(crate::RoomViewLine::State { event });
+				}
+
+				for event in room.timeline.events {
+					lines.push(crate::RoomViewLine::Timeline { event });
+				}
 			}
 
-			for event in room.timeline.events {
-				lines.push(crate::RoomViewLine::Timeline { event });
-			}
-		}
+			for (room_id, room) in rooms.leave {
+				let lines = to_write.entry(room_id).or_default();
 
-		for (room_id, room) in sync.rooms.leave {
-			let lines = to_write.entry(room_id).or_default();
+				lines.push(crate::RoomViewLine::AccountData { account_data: room.account_data });
 
-			lines.push(crate::RoomViewLine::AccountData { account_data: room.account_data });
+				for event in room.state.events {
+					lines.push(crate::RoomViewLine::State { event });
+				}
 
-			for event in room.state.events {
-				lines.push(crate::RoomViewLine::State { event });
-			}
-
-			for event in room.timeline.events {
-				lines.push(crate::RoomViewLine::Timeline { event });
+				for event in room.timeline.events {
+					lines.push(crate::RoomViewLine::Timeline { event });
+				}
 			}
 		}
 
